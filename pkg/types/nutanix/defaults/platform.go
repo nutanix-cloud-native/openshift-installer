2e529b8582e5320a5100e6ddfe9870ed9fe79d23
Sid Shukla <sid.shukla@nutanix.com>
2022-01-14 17:35:11 +0530

    Add the package defaults for Nutanix platform
    This enables us to set defaults for nutanix later down the line.

---
+++ pkg/types/nutanix/defaults/platform.go
@@ -0,0 +1,10 @@
+package defaults
+
+import (
+	"github.com/openshift/installer/pkg/types"
+	"github.com/openshift/installer/pkg/types/nutanix"
+)
+
+
+// SetPlatformDefaults sets the defaults for the platform.
+func SetPlatformDefaults(p *nutanix.Platform, installConfig *types.InstallConfig) {}
