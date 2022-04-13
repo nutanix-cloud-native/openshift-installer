d96f7f999aadbd021d4e9050193caa5a91db831c
Sid Shukla <sid.shukla@nutanix.com>
2022-04-14 00:36:02 +0200

    use uri for image

--- pkg/tfvars/nutanix/nutanix.go
+++ pkg/tfvars/nutanix/nutanix.go
@@ -6,25 +6,24 @@ import (
 	nutanixapis "github.com/openshift/machine-api-provider-nutanix/pkg/apis/nutanixprovider/v1beta1"
 	"github.com/pkg/errors"
 
-	"github.com/openshift/installer/pkg/tfvars/internal/cache"
 	nutanixtypes "github.com/openshift/installer/pkg/types/nutanix"
 )
 
 type config struct {
-	PrismCentralAddress            string `json:"nutanix_prism_central_address"`
-	Port                           string `json:"nutanix_prism_central_port"`
-	Username                       string `json:"nutanix_username"`
-	Password                       string `json:"nutanix_password"`
-	MemoryMiB                      int64  `json:"nutanix_control_plane_memory_mib"`
-	DiskSizeMiB                    int64  `json:"nutanix_control_plane_disk_mib"`
-	NumCPUs                        int64  `json:"nutanix_control_plane_num_cpus"`
-	NumCoresPerSocket              int64  `json:"nutanix_control_plane_cores_per_socket"`
-	PrismElementUUID               string `json:"nutanix_prism_element_uuid"`
-	SubnetUUID                     string `json:"nutanix_subnet_uuid"`
-	Image                          string `json:"nutanix_image"`
-	ImageFilePath                  string `json:"nutanix_image_filepath"`
-	BootstrapIgnitionImage         string `json:"nutanix_bootstrap_ignition_image"`
-	BootstrapIgnitionImageFilePath string `json:"nutanix_bootstrap_ignition_image_filepath"`
+	PrismCentralAddress       string `json:"nutanix_prism_central_address"`
+	Port                      string `json:"nutanix_prism_central_port"`
+	Username                  string `json:"nutanix_username"`
+	Password                  string `json:"nutanix_password"`
+	MemoryMiB                 int64  `json:"nutanix_control_plane_memory_mib"`
+	DiskSizeMiB               int64  `json:"nutanix_control_plane_disk_mib"`
+	NumCPUs                   int64  `json:"nutanix_control_plane_num_cpus"`
+	NumCoresPerSocket         int64  `json:"nutanix_control_plane_cores_per_socket"`
+	PrismElementUUID          string `json:"nutanix_prism_element_uuid"`
+	SubnetUUID                string `json:"nutanix_subnet_uuid"`
+	Image                     string `json:"nutanix_image"`
+	ImageFileURI              string `json:"nutanix_image_uri"`
+	BootstrapIgnitionImage    string `json:"nutanix_bootstrap_ignition_image"`
+	BootstrapIgnitionImageURI string `json:"nutanix_bootstrap_ignition_image_uri"`
 }
 
 // TFVarsSources contains the parameters to be converted into Terraform variables
@@ -41,10 +40,10 @@ type TFVarsSources struct {
 
 //TFVars generate Nutanix-specific Terraform variables
 func TFVars(sources TFVarsSources) ([]byte, error) {
-	cachedImage, err := cache.DownloadImageFile(sources.ImageURL)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to use cached nutanix image")
-	}
+	//cachedImage, err := cache.DownloadImageFile(sources.ImageURL)
+	//if err != nil {
+	//	return nil, errors.Wrap(err, "failed to use cached nutanix image")
+	//}
 
 	bootstrapIgnitionImagePath, err := nutanixtypes.CreateBootstrapISO(sources.ClusterID, sources.BootstrapIgnitionData)
 	if err != nil {
@@ -54,20 +53,20 @@ func TFVars(sources TFVarsSources) ([]byte, error) {
 	bootstrapIgnitionImageName := nutanixtypes.BootISOImageName(sources.ClusterID)
 	controlPlaneConfig := sources.ControlPlaneConfigs[0]
 	cfg := &config{
-		Port:                           sources.Port,
-		PrismCentralAddress:            sources.PrismCentralAddress,
-		Username:                       sources.Username,
-		Password:                       sources.Password,
-		MemoryMiB:                      controlPlaneConfig.MemorySizeMib,
-		DiskSizeMiB:                    controlPlaneConfig.DiskSizeMib,
-		NumCPUs:                        controlPlaneConfig.NumSockets,
-		NumCoresPerSocket:              controlPlaneConfig.NumVcpusPerSocket,
-		PrismElementUUID:               controlPlaneConfig.ClusterReferenceUUID,
-		SubnetUUID:                     controlPlaneConfig.SubnetUUID,
-		Image:                          controlPlaneConfig.ImageName,
-		ImageFilePath:                  cachedImage,
-		BootstrapIgnitionImage:         bootstrapIgnitionImageName,
-		BootstrapIgnitionImageFilePath: bootstrapIgnitionImagePath,
+		Port:                      sources.Port,
+		PrismCentralAddress:       sources.PrismCentralAddress,
+		Username:                  sources.Username,
+		Password:                  sources.Password,
+		MemoryMiB:                 controlPlaneConfig.MemorySizeMib,
+		DiskSizeMiB:               controlPlaneConfig.DiskSizeMib,
+		NumCPUs:                   controlPlaneConfig.NumSockets,
+		NumCoresPerSocket:         controlPlaneConfig.NumVcpusPerSocket,
+		PrismElementUUID:          controlPlaneConfig.ClusterReferenceUUID,
+		SubnetUUID:                controlPlaneConfig.SubnetUUID,
+		Image:                     controlPlaneConfig.ImageName,
+		ImageFileURI:              sources.ImageURL,
+		BootstrapIgnitionImage:    bootstrapIgnitionImageName,
+		BootstrapIgnitionImageURI: bootstrapIgnitionImagePath,
 	}
 	return json.MarshalIndent(cfg, "", "  ")
 }
